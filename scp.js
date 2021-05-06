d1d692faf5b09007bd30235f44685f5e242f90b0
PaoConcepcion <paolaconcepcion8@gmail.com>
2021-05-06 18:22:14 -0500

    Card page v3

--- scp.js
+++ scp.js
@@ -1,51 +1,51 @@
 // JavaScript source code
 var prin = [
-    "Anna",
-    "Aurora",
-    "Mulan",
-    "Alicia",
-    "Blanca Nieves",
-    "Rapunzel",
-    "Jasm\u00EDn",
-    "Ariel",
-    "Cenicienta",
-    "Tiana",
-    "M\u00E9rida",
-    "Bella",
-    "Elsa",
+    "El señor de los anillos",
+    "El extraño mundo de Jack",
+    "Shrek",
+    "Moana",
+    "Star wars",
+    "Charlie y la fabrica de chocolates",
+    "Iron Man",
+    "Harry potter",
+    "Contracara",
+    "La naranja mecánica",
+    "Batman",
+    "Piratas del caribe",
 ];
 
 var desc = [
-    "Anna es una feliz y despreocupada princesa. ",
-    "Criada oculta en el bosque bajo la tutela de 3 hadas. ",
-    "Procedente de China, Mulan es la m\u00E1s divertida e intr\u00E9pida de las Princesas Disney. ",
-    "Alicia es una ni\u00F1a linda, dulce, amable y hermosa que vive en la era de la Inglaterra victoriana. ",
-    "Una hermosa princesita que al intentar escapar de la reina malvada. ",
-    "Naci\u00F3 como princesa, y fue retirada de la custodia de sus padres cuando era un beb\u00E9. ",
-    "Jasm\u00EDn, de raza árabe, una muchacha de belleza ex\u00F3tica e ind\u00F3mita.",
-    "Ariel, una sirena adolescente, sue\u00F1a con vivir en la superficie.",
-    "Cenicienta vive con su madrastra y sus dos hermanastras. ",
-    "Su historia se desarrolla en un per\u00EDodo de tiempo que es m\u00E1s cercano a nuestros d\u00EDas (Nueva Orleans en los a\u00F1os 1920).",
-    "Al inicio M\u00E9rida se comporta como una adolescente mimada. ",
-    "Bella es una joven muy hermosa espiritual e intelectualmente. ",
-    "Elsa, es portadora de poderes criog\u00E9nicos."
+    "La trilogía cinematográfica de El Señor de los Anillos, basada en la novela homónima del escritor británico J. R. R. Tolkien. ",
+    "The Nightmare Before Christmas es una película de fantasía oscura animada en 1993, dirigida por Henry Selick, producida y concebida por Tim Burton. ",
+    "Shrek es una película animada estadounidense de 2001, dirigida por el neozelandés Andrew Adamson y la estadounidense Vicky Jenson. ",
+    "Moana es una película de animación por computadora de Walt Disney Animation Studios que se estrenó el 23 de noviembre de 2016 en Estados Unidos. ",
+    "Star Wars, conocida también en español como La guerra de las galaxias,​ es una franquicia compuesta primordialmente de una serie de películas concebidas por el cineasta estadounidense George Lucas en la década de 1970 ",
+    "Charlie y la fábrica de chocolate (Charlie and the Chocolate Factory en inglés) es una película estrenada en el año 2005, dirigida por Tim Burton.  ",
+    "Iron Man titulada Iron Man: el hombre de hierro en Hispanoamérica, es la primera entrega del Universo cinematográfico de Marvel. ",
+    "La serie cinematográfica de Harry Potter comprende películas basadas en Harry Potter, una serie de novelas juveniles escritas por la autora británica J. K. Rowling y protagonizadas por el mago ficticio del mismo nombre. ",
+    "Face/Off es una película de acción dirigida por John Woo de 1997 y protagonizada por John Travolta y Nicolas Cage. ",
+    "A Clockwork Orange (La naranja mecánica en Hispanoamérica y España) es una película angloestadounidense de ciencia ficción de 1971, producida y dirigida por Stanley Kubrick. ",
+    "The Dark Knight (titulada El caballero oscuro en España y Batman: El caballero de la noche en Hispanoamérica) es una película de superhéroes británico-estadounidense de 2008 dirigida por Christopher Nolan. ",
+    "Piratas del Caribe (en inglés, Pirates of the Caribbean) es el título de una franquicia cinematográfica de aventura fantástica y piratas, producida por Jerry Bruckheimer. ",
+    "Hombres de negro1​  es una película de ciencia ficción del año 1997 protagonizada por Tommy Lee Jones y Will Smith. Narra la historia de una organización secreta que vigila la actividad alienígena en la Tierra."
 ];
 
 var mas = [
-    "Su hermana Elsa lucha por tratar de liberarse a s\u00ED misma de sus miedos, con la ayuda de su hermana peque\u00F1a.",
-    "Aurora es la hermosa joven hija del Rey que es v\u00EDctima de un hechizo de la perversa y malvada Mal\u00E9fica, cayendo en un profundo sueño, del cual solo despierta por un beso del Pr\u00EDncipe Felipe.",
-    "Por su esp\u00EDritu apasionado hace que su familia corra el peligro de caer en deshonor. Vive sus aventuras junto a sus amigos animales y su pequeño y fiel drag\u00F3n.",
-    "Su mascota es un gatito llamado Dinah, y ella tiene una hermana mayor.",
-    "Se cruza con los siete enanos, los animales del bosque y, el apuesto pr\u00EDncipe; quienes compartir\u00E1n una hermosa amistad y amor junto a ella.",
-    "S\u00F3lo para reunirse con ellos en su adolescencia gracias a que decidi\u00F3 seguir sus sue\u00F1os. ",
-    "Desea casarse por amor y ver como es la vida fuera de palacio, cosa que logra gracias al Pr\u00EDncipe Ali.",
-    "Sus amigos el cangrejo Sebastian y el pez Flounder. Su principal enemiga es la malvada bruja del mar, Ursula.",
-    "Ellas la obligan a trabajar el d\u00EDa entero, por lo que sue\u00F1a con liberarse y encontrar el amor. Y esto lo logra gracias a su hada madrina.",
-    "A sus 19 a\u00F1os, Tiana se convierte en princesa despu\u00E9s de casarse con el Pr\u00EDncipe Naveen, el hijo mayor y heredero del rey de Maldonia.",
-    "Pero al transcurso de la pel\u00EDcula aprende que su comportamiento no fue el ideal y que debe enmendar sus errores.",
-    "Por lo que cuando vive en la mansi\u00F3n de la Bestia, el amor genera que el maleficio que pose\u00EDa el pr\u00EDncipe lo convierta en humano nuevamente y encuentre la felicidad so\u00F1ada.",
-    "Elsa involuntariamente crean un eterno invierno el d\u00EDa de su coronaci\u00F3n."
+    "Comprende tres películas épicas de fantasía, acción y aventuras: El Señor de los Anillos: la Comunidad del Anillo (2001), El Señor de los Anillos: las dos torres (2002) y El Señor de los Anillos: el retorno del Rey (2003).",
+    "Cuenta la historia de Jack Skellington, el Rey de la Ciudad de Halloween, que tropieza a través de un portal a la Ciudad de la Navidad y decide celebrar la fiesta.",
+    "La película ganó el Óscar a la mejor película animada y participó en la selección oficial del Festival de Cine de Cannes de 2001.​ Forma parte del AFI's 10 Top 10 en la categoría películas de animación.​ En 2020, fue seleccionada para su preservación en el National Film Registry de la Biblioteca del Congreso de los Estados Unidos.",
+    "Se ha dicho que la historia se desarrollará en unas islas en el Océano Pacífico, la protagonista será la joven Moana, una chica aventurera que se adentrará en el Océano.",
+    ", y producidas y distribuidas por The Walt Disney Company a partir de 2012. Su trama describe las vivencias de un grupo de personajes que habitan en una galaxia ficticia e interactúan con elementos como «la Fuerza», un campo de energía metafísico y omnipresente9​ que posee un «lado luminoso» impulsado por la sabiduría, la nobleza y la justicia y utilizado por Jedi, y un «lado oscuro» usado por los Sith y provocado por la ira, el miedo y el odio.",
+    "Es la segunda adaptación cinematográfica de la novela homónima escrita por Roald Dahl en 1964, y está protagonizada por Johnny Depp como Willy Wonka y Freddie Highmore como Charlie Bucket. ",
+    "Cuando Stark es capturado en territorio enemigo, construye una armadura de alta tecnología para escapar. Ahora, él está en una misión para salvar el mundo como un héroe que no nació para ser diferente a cualquier otro.",
+    "Se describen las aventuras del joven aprendiz de magia y hechicería Harry Potter y sus amigos Hermione Granger y Ron Weasley, durante los años que pasan en el Colegio Hogwarts de Magia y Hechicería.",
+    "La misma cuenta cómo un agente del FBI asume la apariencia física de su peor enemigo con el objetivo de parar un plan terrorista, y al mismo tiempo cómo el enemigo luego se convierte en él.",
+    "La película se caracteriza por contenidos violentos que facilitan una crítica social en psiquiatría, el pandillerismo juvenil, las teorías conductistas en psicología y otros tópicos ubicados en una sociedad futurista distópica.",
+    "Basada en el personaje Batman de DC Comics, la película es la segunda parte de la serie fílmica Batman de Nolan y una secuela de Batman Begins de 2005, protagonizada por Christian Bale, Michael Caine, Heath Ledger, Gary Oldman, Aaron Eckhart, Maggie Gyllenhaal y Morgan Freeman.",
+    "Esta saga de películas se basa libremente en mitos y leyendas de los mares, como el pirata Davy Jones y su tripulación fantasmal a bordo de El holandés errante; la mitología griega que incluye dioses como Poseidón, la ninfa Calipso, monstruos como el Kraken, sirenas, brujería y la vida de los piratas de la época.",
+    "La película es una adaptación de la historieta homónima de Lowell Cunningham. A su vez, el título hace referencia a los Hombres de negro, unos personajes habituales en las teorías de la conspiración que supuestamente serían los encargados de ocultar a los ciudadanos la presencia de extraterrestres en la Tierra."
 ];
+
 
 window.onload = function() {
 
